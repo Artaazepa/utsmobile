3c59a8acbd8c4dc3e205262477cefcda5a5cb7fe
Artaazepa <artaazepa@gmail.com>
2024-10-28 11:12:13 +0700

    first commit

---
+++ app/src/main/java/uts/arta/tasks/AgendaRepository.java
@@ -0,0 +1,26 @@
+// AgendaRepository.java
+package uts.arta.tasks;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class AgendaRepository {
+    private AgendaDAO agendaDAO;
+
+    public AgendaRepository(Context context) {
+        agendaDAO = new AgendaDAO(context);
+    }
+
+    public long addAgenda(Agenda agenda) {
+        return agendaDAO.addAgenda(agenda);
+    }
+
+    public List<Agenda> getAllAgendas() {
+        return agendaDAO.getAllAgendas();
+    }
+
+    public void deleteAgenda(int id) {
+        agendaDAO.deleteAgenda(id);
+    }
+}
---
+++ app/src/main/java/uts/arta/tasks/item_agenda.java
@@ -0,0 +1,14 @@
+package uts.arta.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_agenda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_agenda);
+    }
+}
